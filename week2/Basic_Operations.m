8041dd0675b5556cbd4ba08af0b1eb4464187e96
liyikun <machine-learning@qq.com>
2018-10-18 21:41:03 +0800

    Add Code in Second Week

---
+++ week2/Basic_Operations.m
@@ -0,0 +1,53 @@
+5 + 6
+3 - 2
+5 * 8
+1 / 2
+2 ^ 6
+1 == 2
+1 ~= 2
+1 && 0
+1 || 0
+xor(1, 0)
+% PS1('>> ');
+a = 3
+a = 3; % semicolon supressing output
+b = 'hi';
+b
+c = (3 >= 1);
+c
+a = pi;
+a
+disp(a);
+fprintf('2 decimals: %0.2f\n', a);
+fprintf('6 decimals: %0.6f\n', a);
+format long
+a
+format short
+a
+A = [1 2; 3 4; 5 6]
+A = [1 2;
+     3 4;
+     5 6]
+V = [1, 2, 3]
+V = [1; 2; 3]
+V = 1 : 0.1 : 2
+V = 1 : 6
+ones(2, 3)
+C = 2 * ones(2, 3)
+C = [2 2 2; 2 2 2]
+W = ones(1, 3)
+W = zeros(1, 3)
+W = rand(1, 3)
+rand(3, 3)
+rand(3, 3)
+rand(3, 3)
+W = -6 + sqrt(10) * (randn(1, 10000));
+hist(W)
+hist(W, 50)
+eye(4)
+eye(6)
+eye(3)
+help eye
+help rand
+help randn
+help help
